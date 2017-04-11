50f3e45bd4a3bcac034b4bce6ffac1feea3b2e3e
Parham Pourdavood <ppourdavood@gmail.com>
2017-04-11 12:25:11 -0700

    Fix disjunctive reasoning

--- Propositional_Logic_Interpreter/main.py
+++ Propositional_Logic_Interpreter/main.py
@@ -1,5 +1,7 @@
 import re
 
+knowledge_dict = dict()
+
 
 class Expression:
 
@@ -41,6 +43,15 @@ class Expression:
 
         elif self.operator_recognizer() == "OR":
             parsed_expression = self.expression.split(" OR ")
+            for i in parsed_expression:
+                if "((" in i:
+                    list_index = parsed_expression.index(i)
+                    i = i[1:]
+                    parsed_expression[list_index] = i
+                elif "))" in i:
+                    list_index = parsed_expression.index(i)
+                    i = i[:-1]
+                    parsed_expression[list_index] = i
             return parsed_expression
         elif self.operator_recognizer() == "Conditional":
             conditional_matched = re.match(self.or_regex, self.expression)
@@ -55,9 +66,85 @@ class Expression:
                 return False
         return True
 
+    def resolver(self, knowledge_dict):
+        if self.operator_recognizer() == "AND":
+            parsed_expression = self.expression_parser()
+            for expression in parsed_expression:
+                if expression not in knowledge_dict:
+                    knowledge_dict[expression] = None
+            for expression in parsed_expression:
+                if knowledge_dict[expression] is None:
+                    return None
+            for expression in parsed_expression:
+                if knowledge_dict[expression]:
+                    return True
+            return False
+        elif self.operator_recognizer() == "OR":
+            parsed_expression = self.expression_parser()
+            for expression in parsed_expression:
+                if expression not in knowledge_dict:
+                    knowledge_dict[expression] = None
+            # count to check if all elements are false
+            count = True
+            for expression in parsed_expression:
+                if knowledge_dict[expression] is True:
+                    return True
+                if knowledge_dict[expression] is False:
+                    count += 1
+            if count == len(parsed_expression):
+                return False
+            else:
+                return None
 
-knowledge_dict = dict()
+
+
+    # def or_resolver(self, knowledge_dict):
+    #     parsed_expression = self.expression_parser()
+    #     for expression in parsed_expression:
+    #         if expression not in knowledge_dict:
+    #             knowledge_dict[expression] = None
+    #     for expression in parsed_expression:
+    #         if knowledge_dict[expression] is None:
+    #             return None
+    #     for expression in parsed_expression:
+    #         if knowledge_dict[expression]:
+    #             return True
+    #     return False
+    #
+    # def and_resolver(self, knowledge_dict):
+    #     parsed_expression = self.expression_parser()
+    #     for expression in parsed_expression:
+    #         if expression not in knowledge_dict:
+    #             knowledge_dict[expression] = None
+    #     for expression in parsed_expression:
+    #         if expression is True:
+    #             return True
+    #     return False
+
 
 def interpreter(expression):
     expression_object = Expression(expression)
-    parsed_object = expression_object.expression_parser()
+    if expression_object.operator_recognizer() == "Pure":
+        knowledge_dict[expression] = True
+    elif expression_object.is_pure_proposition():
+        knowledge_dict[expression] = expression_object.resolver(knowledge_dict)
+    else:
+        parsed_expression = expression_object.expression_parser()
+        # if expression not in knowledge_dict:
+        #     knowledge_dict[expression] = None
+        for i in parsed_expression:
+            if i not in knowledge_dict:
+                knowledge_dict[i] == None
+        knowledge_dict[expression] = expression_object.resolver(knowledge_dict)
+        for i in parsed_expression:
+            temp_expression_object = Expression(i)
+            expression_type = temp_expression_object.operator_recognizer()
+            if expression_type != "Pure" and expression_type != "Broken":
+                interpreter(i)
+
+interpreter("(I study)")
+
+
+interpreter("(I go to school) OR ((I play football) OR ((I Dance)) OR (I study))")
+
+print(knowledge_dict)
